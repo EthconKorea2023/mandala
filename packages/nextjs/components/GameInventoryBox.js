1a8c99c955e4ad341e5297826282e10b53e54874
smarthug <kirklayer@gmail.com>
2023-09-02 15:32:45 +0900

    one ring done

--- packages/nextjs/components/GameInventoryBox.js
+++ packages/nextjs/components/GameInventoryBox.js
@@ -1,9 +1,12 @@
 import { createRef, useEffect, useMemo, useState } from "react";
 // import dynamic from "next/dynamic";
-import { Box, ListItemText, Paper, paperClasses, styled } from "@mui/material";
+import { ListItemText, Paper, paperClasses, styled } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import { createPortal } from "react-dom";
+import { isOwnRing } from "~~/utils/mandala/utils";
+
+import useEnvStore from "~~/utils/store/envStore";
 
 // const Droppable = dynamic(
 //   () =>
@@ -72,15 +75,11 @@ function _optionalPortal(styles, element) {
 }
 
 export default function GameInventoryBox() {
-  const [rawItems, setItems] = useState([
-    {
-      name: "The One Ring",
-      description:
-        "Three Rings for the Elven-kings under the sky,Seven for the Dwarf-lords in their halls of stone,Nine for Mortal Men doomed to die,One for the Dark Lord on his dark throneIn the Land of Mordor where the Shadows lie.   One Ring to rule them all, One Ring to find them,   One Ring to bring them all, and in the darkness bind themIn the Land of Mordor where the Shadows lie.",
-      image: "https://nftstorage.link/ipfs/bafybeid6bnsatz56fkgb4hldqtrhw3hqs7niyp5uvqcbwx6u5pre5uwzze",
-      glb: "https://nftstorage.link/ipfs/bafybeib4wicygqdinah6kwnur65q5muekzlj7i7rtif4hkfx7oce6bfu3u",
-    },
-  ]);
+  const [selectedGame, setGame] = useEnvStore(state => [state.selectedGame, state.setGame]);
+  const [characterTBAArr, setCharacterTBAArr] = useEnvStore(state => [state.characterTBAArr, state.setCharacterTBAArr]);
+  const [rawItems, setItems] = useState([]);
+
+
   const items = useMemo(() => {
     if (rawItems.length >= 9) return rawItems;
     const minArray = new Array(9).fill(undefined);
@@ -89,6 +88,34 @@ export default function GameInventoryBox() {
     }
     return minArray;
   }, [rawItems]);
+
+  useEffect(() => {
+
+
+    async function checkRing() {
+
+      const currenSelectedTBA = characterTBAArr[selectedGame];
+      console.log(currenSelectedTBA);
+
+      const hasRing = await isOwnRing(currenSelectedTBA);
+      console.log(hasRing)
+
+      if (hasRing) {
+        setItems([{
+          name: "The One Ring",
+          description:
+            "Three Rings for the Elven-kings under the sky,Seven for the Dwarf-lords in their halls of stone,Nine for Mortal Men doomed to die,One for the Dark Lord on his dark throneIn the Land of Mordor where the Shadows lie.   One Ring to rule them all, One Ring to find them,   One Ring to bring them all, and in the darkness bind themIn the Land of Mordor where the Shadows lie.",
+          image: "https://nftstorage.link/ipfs/bafybeid6bnsatz56fkgb4hldqtrhw3hqs7niyp5uvqcbwx6u5pre5uwzze",
+          glb: "https://nftstorage.link/ipfs/bafybeib4wicygqdinah6kwnur65q5muekzlj7i7rtif4hkfx7oce6bfu3u",
+        }])
+      } else {
+        setItems([])
+      }
+    }
+
+    checkRing();
+
+  }, [selectedGame])
   useEffect(() => {
     // // Fetching Items from chain by game
     // setItems()
@@ -98,6 +125,8 @@ export default function GameInventoryBox() {
     document.body.appendChild(_portalIn);
 
     _portal.current = _portalIn;
+
+
 
     return () => {
       document.body.removeChild(_portalIn);
@@ -109,7 +138,7 @@ export default function GameInventoryBox() {
       <Droppable droppableId="inven" key={`gmae-in}`}>
         {(provided, snapshot) => (
           <StyledBox ref={provided.innerRef} elevation={10} {...provided.droppableProps}>
-            {items.map((_item, _idx) =>
+            {items.length > 0 ? items.map((_item, _idx) =>
               _item ? (
                 <Draggable draggableId={_item.name} index={_idx} key={_item.name}>
                   {(provided, snapshot) =>
@@ -136,7 +165,10 @@ export default function GameInventoryBox() {
               ) : (
                 <Paper key={_idx} />
               ),
-            )}
+            ) : (
+              <div>Loading ...</div>
+            )
+            }
             {provided.placeholder}
           </StyledBox>
         )}
