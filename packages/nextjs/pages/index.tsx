8583ffffe94310be6154194af2f8760cc4568b3e
smarthug <kirklayer@gmail.com>
2023-09-02 10:04:32 +0900

    interaction & biconomy dynamic paymaster creation success

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
+import Inventories from "~~/components/inventories";
 
 const Home: NextPage = () => {
   return (
     <>
-      <MetaHeader />
+      <Inventories />
     </>
   );
 };
