f5d3c6de8fae8e7a180a3124b8d67c288519e275
smarthug <kirklayer@gmail.com>
2023-09-02 15:08:05 +0900

    metadata upgraded

--- packages/nextjs/components/GameCard.js
+++ packages/nextjs/components/GameCard.js
@@ -74,16 +74,18 @@ export default function GameCard() {
   const [gameList, setGameList] = useState([
     {
       name: "Gollum",
+      game: "The Lord of the Rings",
       description:
         'Gollum, also known as Sméagol, was a creature (originally a Stoorish Hobbit) who bore the One Ring. He lived in the Misty Mountains for most of his life. In T.A. 2941 he lost the Ring to Bilbo Baggins. For the rest of his life he sought to recover his "precious" "birthday present". In T.A. 3019 he followed the Fellowship of the Ring and met Frodo Baggins. After leading Frodo into Mordor and betraying him to Shelob he finally seized the Ring in Sammath Naur. In his euphoria he died and destroyed the Ring after falling into the fires of Mount Doom.',
       image: "https://nftstorage.link/ipfs/bafybeibhz6tq772eletyup6vwpcngcujpdpruz3cf5gntlrjqn54sq5qei",
-      glb: "https://nftstorage.link/ipfs/bafybeihsqpvgmjbo24odnzrnusndhboz3ar2w35oherex2gwborvjtnfja",
+      glb: "https://nftstorage.link/ipfs/bafybeibfg2lszfe2sj4wxv62z452kzbeqqthrww7jsjydzjuqtbbcul6bq",
     },
     {
       name: "Sample_Character",
+      game: "Sample_Game",
       description: "Sample Game Character for ETHConKR, 2023",
       image: "https://nftstorage.link/ipfs/bafybeie6jknmoybc7l6n6djaznyp7eqjioj66dfosbyj6bcc7qhtrxvjli",
-      glb: "https://nftstorage.link/ipfs/bafybeihuhdlshxzsubub7j2eokh23r6wzxmieu5qp4n7j77xirdbo2pbje",
+      glb: "https://nftstorage.link/ipfs/bafkreihnmcjunr4sp6onalveoj6ahhi4pcfmcaic5lovwvo2kxgauvjtom",
     },
   ]);
 
